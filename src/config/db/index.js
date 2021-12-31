df1bb83f17a954c396ae0c5d328b60f28f20c57f
ledong0110 <74060032+ledong0110@users.noreply.github.com>
2021-12-31 09:46:56 +0700

    Update link database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/dong_blog_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(
+            'mongodb+srv://ledong0110:zyxOvPvnytMiDh0q@cluster0.digrh.mongodb.net/Dong-Dev-Blog?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        );
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect Failure!');
